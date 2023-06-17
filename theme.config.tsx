4ae9a076671baff0f3c05391859764a171794bda
Higor Alves <me@higoralves.dev>
2023-06-16 21:37:48 -0300

    fix: renaming files

--- theme.config.tsx
+++ theme.config.tsx
@@ -1,17 +1,17 @@
 import React from 'react'
-import { DocsThemeConfig } from 'nextra-theme-docs'
+import {DocsThemeConfig} from 'nextra-theme-docs'
 
 const config: DocsThemeConfig = {
-  logo: <span>My Project</span>,
+  logo: <span>Docs.</span>,
   project: {
-    link: 'https://github.com/shuding/nextra-docs-template',
+    link: 'https://github.com/HigorAlves/docs',
   },
   chat: {
     link: 'https://discord.com',
   },
-  docsRepositoryBase: 'https://github.com/shuding/nextra-docs-template',
+  docsRepositoryBase: 'https://github.com/HigorAlves/docs',
   footer: {
-    text: 'Nextra Docs Template',
+    text: 'Higor Alves Docs',
   },
 }
 
